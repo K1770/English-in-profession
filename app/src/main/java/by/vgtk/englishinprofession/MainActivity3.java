801df9e01417ace3a70d3c22c74f1cc2d37e04c5
gruntik <gruntov99@mail.ru>
2021-12-14 11:07:08 +0300

    igor test

--- app/src/main/java/by/vgtk/englishinprofession/MainActivity3.java
+++ app/src/main/java/by/vgtk/englishinprofession/MainActivity3.java
@@ -24,6 +24,7 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         System.out.println("igor test");
+        System.out.println("igor test");
 
         super.onCreate(savedInstanceState);
 
