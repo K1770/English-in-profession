512ec44a4813d1fd0f4a741c6fc692cab8b22b15
gruntik <gruntov99@mail.ru>
2021-12-14 10:53:56 +0300

    Test commit igor 1

--- app/src/main/java/by/vgtk/englishinprofession/fragments/ComputerPartsFragment.java
+++ app/src/main/java/by/vgtk/englishinprofession/fragments/ComputerPartsFragment.java
@@ -1,11 +1,9 @@
 package by.vgtk.englishinprofession.fragments;
 
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -33,22 +31,14 @@ public class ComputerPartsFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+        TextView textView = (TextView)getView().findViewById(R.id.computerTextView);
+
         Bundle arguments = getActivity().getIntent().getExtras();
         String position = arguments.get("position").toString();
 
-        TextView textView = (TextView)getView().findViewById(R.id.computerTextView);
-        ImageView img = (ImageView)getView().findViewById(R.id.imageViewComputer);
-        //textView.setText(Resources.getSystem().getIdentifier(getText(position), "values/strings", "android"));
-        try {
-            textView.setText(R.string.class.getField(getText(position)).getInt(null));
-            img.setImageResource(R.drawable.class.getField(getText(position)).getInt(null));
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        }
-
-
+        textView.setText(getText(position));
     }
 
     @Override
@@ -60,16 +50,16 @@ public class ComputerPartsFragment extends Fragment {
     private String getText(String position) {
         String text;
         switch (position) {
-            case "00" : text = "monitor"; break;
-            case "01" : text = "printer"; break;
-            case "02" : text = "speaker"; break;
-            case "03" : text = "memory"; break;
-            case "04" : text = "keyboard"; break;
-            case "05" : text = "mouse"; break;
-            case "06" : text = "scanner"; break;
-            case "07" : text = "cpu"; break;
-            case "08" : text = "headsets"; break;
-            case "09" : text = "webcam"; break;
+            case "00" : text = getString(R.string.monitor); break;
+            case "01" : text = getString(R.string.printer); break;
+            case "02" : text = getString(R.string.speaker); break;
+            case "03" : text = getString(R.string.memory); break;
+            case "04" : text = getString(R.string.keyboard); break;
+            case "05" : text = getString(R.string.mouse); break;
+            case "06" : text = getString(R.string.scanner); break;
+            case "07" : text = getString(R.string.cpu); break;
+            case "08" : text = getString(R.string.headsets); break;
+            case "09" : text = getString(R.string.webcam); break;
             default : text = ""; break;
         }
         return text;
