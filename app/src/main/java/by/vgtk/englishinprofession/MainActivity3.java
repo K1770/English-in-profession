b551003ceb5fc14f3548bde80c2108b4d7f7f292
gruntik <gruntov99@mail.ru>
2022-01-11 10:27:21 +0300

    delete comment igor

--- app/src/main/java/by/vgtk/englishinprofession/MainActivity3.java
+++ app/src/main/java/by/vgtk/englishinprofession/MainActivity3.java
@@ -23,7 +23,6 @@ public class MainActivity3 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //first commit igor
 
         binding = ActivityMain3Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
